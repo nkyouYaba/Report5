56c90e7277b973b186d425e1034350c18375f3d6
Kyosuke Nakama <e235752@ie.u-ryukyu.ac.jp>
2023-12-09 21:26:55 +0900

    2nd commit

--- src/Error.java
+++ src/Error.java
@@ -1,6 +1,11 @@
 public class Error {
     public static void main(String[] args){
+        try{
         String str = null;
         System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerExceptionが発生しました");
+            System.out.println(e.getMessage());
+        }
     }
 }

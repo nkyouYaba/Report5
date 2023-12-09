32e208a30d222ac87a7a609d94d9719e2953668a
Kyosuke Nakama <e235752@ie.u-ryukyu.ac.jp>
2023-12-10 01:18:05 +0900

    4th commit

--- src/Error.java
+++ src/Error.java
@@ -1,7 +1,13 @@
 public class Error {
     public static void main(String[] args){
+        try{
             String str = "壱百満";
             int value = Integer.parseInt(str);
             System.out.println(value);
+        } catch (NumberFormatException e) {
+            String str = "1000000";
+            int value = Integer.parseInt(str);
+            System.out.println(value);
+        }
     }
 }
